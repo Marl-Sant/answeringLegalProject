3a6f7b079d9babbd9470f5997a37dc040e90016a
Marl-Sant <santosfmarlon@gmail.com>
2025-10-13 10:19:46 -0400

    fixed breaking error

--- app/components/IntakeSectionDesktop.tsx
+++ app/components/IntakeSectionDesktop.tsx
@@ -13,7 +13,7 @@ export default function IntakeSectionDesktop() {
                 <hr className="border-t-[2px] border-[#3CCED7] max-w-[534px] w-full rounded-[8px]" />
                 <p className="max-w-[534px] w-full h-[110px] font-[400] text-[20px] text-[#141414]">Get everything you need out of every new client call. The virtual receptionists at our legal intake call center will use your unique specifications to perform a legal intake for every new client caller.</p>
             </div>
-            <ButtonOrLink href="#pricing" padding="px-[24px] py-[22px]">See our pricing</ButtonOrLink>
+            <ButtonOrLink padding="px-[24px] py-[22px]">See our pricing</ButtonOrLink>
         </div>
         <div className="max-w-[534px] h-[453px] min-w-[364px] w-full mb-[60px]">
             <Image src={receptionist} alt="A customer service representative on the phone" />
